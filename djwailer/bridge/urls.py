b4a058188cb8bde045f49d52d47415da919fd07b
Bender <plungerman@gmail.com>
2015-10-09 15:00:45 -0500

    removed the update option for now; moved redirect to last; unicode test second to last

--- djwailer/bridge/urls.py
+++ djwailer/bridge/urls.py
@@ -3,22 +3,22 @@ from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = patterns('djwailer.bridge.views',
     url(
-        r'^$', RedirectView.as_view(url="/bridge/")
+        r'^(?P<content_type>[\d\w]+)/success/',
+        'submission_success', name="submission_success"
+    ),
+    #url(
+    #    r'^(?P<content_type>[\d\w]+)/(?P<oid>\d+)/',
+    #    'submission_form', name='submission_form'
+    #),
+    url(
+        r'^(?P<content_type>[\d\w]+)/',
+        'submission_form', name='submission_form'
     ),
     url(
         r'^unicode/(?P<oid>\d+)/',
         'unicode_test', name='unicode_test'
     ),
     url(
-        r'^(?P<content_type>[\d\w]+)/success/',
-        'submission_success', name="submission_success"
-    ),
-    url(
-        r'^(?P<content_type>[\d\w]+)/(?P<oid>\d+)/',
-        'submission_form', name='submission_form'
-    ),
-    url(
-        r'^(?P<content_type>[\d\w]+)/',
-        'submission_form', name='submission_form'
+        r'^$', RedirectView.as_view(url="/bridge/")
     ),
 )
