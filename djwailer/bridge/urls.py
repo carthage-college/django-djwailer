868c52533681090711d04bb601336a728b040230
plungerman <plungerman@gmail.com>
2021-04-27 17:04:12 -0500

    fixed urls that spaces in the str arg

--- djwailer/bridge/urls.py
+++ djwailer/bridge/urls.py
@@ -10,10 +10,10 @@ from djwailer.bridge import views
 urlpatterns = [
     path('unicode/<int: oid>)/', views.unicode_test, name='unicode_test'),
     path(
-        '<str: content_type>/success/',
+        '<str:content_type>/success/',
         views.submission_success,
         name='submission_success',
     ),
-    path('<str: content_type>/', views.submission_form, name='submission_form'),
+    path('<str:content_type>/', views.submission_form, name='submission_form'),
     path('', RedirectView.as_view(url="/bridge/")),
 ]
